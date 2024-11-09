089214fa6c1fc6466f8e4bd77c031f8df99199de
KnightPentest <admin@knightpentest.com>
2024-11-09 14:39:43 -0500

    Quartz sync: Nov 9, 2024, 2:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ru-RU",
     baseUrl: "ad.knightpentest.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
