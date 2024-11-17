3b18be6ee0fd04ce1e8983f1d18149079af43972
KnightPentest <admin@knightpentest.com>
2024-11-17 18:43:57 -0500

    Quartz sync: Nov 17, 2024, 6:43 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,22 +1,20 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
-// Components shared across all pages
+// components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [
-    Component.Darkmode(), // Перемещено сюда
-  ],
+  afterBody: [],
   footer: Component.Footer({
     links: {
-      Chat: "https://t.me/greyhatchat",
+      "Chat": "https://t.me/greyhatchat",
       "Telegram blog": "https://t.me/mirea",
     },
   }),
 }
 
-// Components for pages that display a single page (e.g., a single note)
+// components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -28,27 +26,25 @@ export const defaultContentPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Explorer(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
-    Component.TableOfContents(),
+    Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
 
-// Components for pages that display lists of pages (e.g., tags or folders)
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-  ],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Explorer(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
 }
